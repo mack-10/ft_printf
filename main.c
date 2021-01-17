b0a53e537f2ef1395ae2776028d977c9041ab781
Sujin Jeon <sujeon@c4r7s2.42seoul.kr>
2021-01-17 21:46:28 +0900

    Xx check

--- main.c
+++ main.c
@@ -2,11 +2,20 @@
 
 int main(void)
 {
-	int a = 22;
+	int a = 10;
 	/*printf("\n");
 	printf("this %u number\n", 2147483648);
 	ft_printf("this %u number", 2147483648);
 	printf("\n\n");*/
+
+	printf("----------X----------\n");
+	printf("flag -\t\t\t:%-6X\n", a);
+	printf("flag 0\t\t\t:%-6X\n", a);
+	printf("flag complex\t:%0-6X\n", a);
+	printf("w=6\t\t\t\t:%6X\n", a);
+	printf("p=6\t\t\t\t:%.6X\n", a);
+	printf("w=6/p=2\t\t\t:%6.2X\n", a);
+	printf("w=6/p=5\t\t\t:%6.5X\n", a);
 
 	printf("----------x----------\n");
 	printf("flag -\t\t\t:%-6x\n", a);
@@ -15,7 +24,5 @@ int main(void)
 	printf("w=6\t\t\t\t:%6x\n", a);
 	printf("p=6\t\t\t\t:%.6x\n", a);
 	printf("w=6/p=2\t\t\t:%6.2x\n", a);
-	printf("w=6/p=5\t\t\t:%6.5x\n", a);
-
 	return (0);
 }
